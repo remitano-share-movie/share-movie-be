2c9e072d6c4525bf7268115ee8b64fd8c00b27c6
ngdkhoi <ngdkhoi27600@gmail.com>
2023-04-13 11:13:36 +0700

    first commit

---
+++ tests/userTest.test.js
@@ -0,0 +1,11 @@
+import request from 'supertest'
+const app = require('../app')
+
+describe("User authentications", () => {
+  test("1 plus 1 should be 2", () => {
+    let a = 1
+    let b = 1
+    expect(a+b).toBe(2)
+  })
+})
+
